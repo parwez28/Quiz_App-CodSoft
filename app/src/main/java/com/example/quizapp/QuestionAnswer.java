91bd0456f334913f319a2d5d16033b8a82108dd2
Parwez <parwezak5050@gmail.com>
2024-06-21 07:49:43 +0530

    This is our Quiz App

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "When the metal reacts with dilute acid, which gas is formed?",
+            "Right to emergency medical aid is a?",
+            "Digestion of food in human beings begins in which part of the alimentary canal?",
+            "The green planet in the solar system is?",
+            "Where was India’s first national Museum opened?",
+            "Which among the following controls all parts of the computer and known as the brain of computer?",
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which of the following is responsible for nitrogen fixation?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Carbon Dioxide","Helium","Neon","Hydrogen"},
+            {"Legal Right","Illegal Right","Constitutional Right","Fundamental Right"},
+            {"Liver","Kidney","Mouth","Large intestine"},
+            {"Mars","Uranus","Venus","Earth"},
+            {"Delhi","Hyderabad","Rajasthan","Mumbai"},
+            {"RAM","ROM","CPU","Processor"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Fungus","Bacteria","Virus","Insects"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Hydrogen",
+            "Fundamental Right",
+            "Mouth",
+            "Uranus",
+            "Mumbai",
+            "CPU",
+            "Google",
+            "Notepad",
+            "Bacteria",
+            "Apple"
+    };
+}
